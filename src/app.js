edb082d197385e4b181a06d73bc0a00fab07be02
Jameel Sikandar <jameelsikandarr@gmail.com>
2025-07-09 01:53:19 +0500

    import and declare router

--- src/app.js
+++ src/app.js
@@ -19,6 +19,14 @@ app.use(express.static("public"))
 
 app.use(cookieParser())
 
+// import router
+
+import userRouter from './routes/user.routes.js'
+
+// routes declaration
+
+app.use('/api/v1/users', userRouter)
+
 
 
 
