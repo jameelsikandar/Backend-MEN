086b898f4ef7330bfb9cd0556adf41bf926068b9
Jameel Sikandar <jameelsikandarr@gmail.com>
2025-07-08 18:50:23 +0500

    upload to cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+import { v2 as cloudinary } from 'cloudinary'
+import fs from 'fs'
+
+cloudinary.config(
+    {
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET
+    }
+)
+
+
+const uploadOnCloudinary = async function (localfilepath) {
+    try {
+        if (!localfilepath) return null;
+
+        //upload file on cloudinary
+        const response = await cloudinary.uploader.upload(localfilepath, {
+            resource_type: 'auto'
+        })
+
+        //file has been uploaded
+        console.log("File has been uploaded on cloudinary", response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localfilepath) // remove the locally saved temp file as the upload operation failed
+        return null;
+    }
+}
+
+
+export { uploadOnCloudinary }
