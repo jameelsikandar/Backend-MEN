22206955df4351ef5bc69f46d09ff00ba32fbaa9
Jameel Sikandar <jameelsikandarr@gmail.com>
2025-07-11 18:37:17 +0500

    get current user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -259,9 +259,11 @@ const changeUserPassword = asyncHandler(async (req, res) => {
 })
 
 const getCurrentUser = asyncHandler(async (req, res) => {
-
+    return res
+        .status(200)
+        .json(200, req.user, "Current user fetched successfully")
 })
 
 
 
-export { registerUser, loginUser, logoutUser, refreshAccessToken }; 
+export { registerUser, loginUser, logoutUser, refreshAccessToken, changeUserPassword, getCurrentUser }; 
