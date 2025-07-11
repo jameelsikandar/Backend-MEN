a2ad048547814e6e8ab40832eeb013834637fcdf
Jameel Sikandar <jameelsikandarr@gmail.com>
2025-07-11 15:00:38 +0500

    Change user password function added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -239,6 +239,29 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 
 })
 
+// change user password
+
+const changeUserPassword = asyncHandler(async (req, res) => {
+    const { oldPassword, newPassword } = req.body;
+
+    const user = await User.findById(req.user?.id)
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+    if (!isPasswordCorrect) {
+        throw new ApiError(400, "Old Password is wrong")
+    }
+
+    user.password(newPassword)
+    await user.save({ validateBeforeSave: false })
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, {}, "Password changed successfully"))
+})
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+
+})
+
 
 
 export { registerUser, loginUser, logoutUser, refreshAccessToken }; 
