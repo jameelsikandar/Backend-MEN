52ba4975ca57499e40dc9701235b68f5dcd90ce8
Jameel Sikandar <jameelsikandarr@gmail.com>
2025-07-14 11:24:49 +0500

    fixed bug in refreshAccessToken

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -204,7 +204,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
             process.env.REFRESH_TOKEN_SECRET
         )
 
-        const user = User.findById(decodedToken?._id)
+        const user = await User.findById(decodedToken?._id)
 
         if (!user) {
             throw new ApiError(401, "Invalid refresh access token")
